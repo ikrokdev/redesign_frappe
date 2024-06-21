1555d5c1e719410b24f65c1ac7ad5fd7c39ab572
Roman <roman.s@ikrok.net>
2024-06-21 07:11:50 +0000

    feature(quick-list-widget): add new button to add/remove fields in quick lists

--- frappe/public/js/frappe/widgets/quick_list_widget.js
+++ frappe/public/js/frappe/widgets/quick_list_widget.js
@@ -1,3 +1,4 @@
+import ListSettings from "../../../../../../../sites/assets/frappe/js/frappe/list/list_settings.js";
 import Widget from "./base_widget.js";
 
 frappe.provide("frappe.utils");
@@ -19,9 +20,11 @@ export default class QuickListWidget extends Widget {
 	set_actions() {
 		if (this.in_customize_mode) return;
 
-		this.setup_add_new_button();
-		this.setup_refresh_list_button();
-		this.setup_filter_list_button();
+		this.setup_fields_list_button().then(() => {
+			this.setup_add_new_button();
+			this.setup_refresh_list_button();
+			this.setup_filter_list_button();
+		});
 	}
 
 	setup_add_new_button() {
@@ -59,6 +62,210 @@ export default class QuickListWidget extends Widget {
 		});
 	}
 
+	async setup_fields_list_button() {
+		this.fields_list = $(
+			`<div class="fields-list btn btn-xs pull-right" title="${__("Add/Update Fields")}">
+				${frappe.utils.icon("dashboard", "sm")}
+			</div>`
+		);
+
+		this.fields_list.appendTo(this.action_area);
+		//v2
+		this.meta = await frappe.db.get_doc('DocType', this.document_type).then((data) => data);
+		await this.get_list_view_settings();
+		await this.setup_columns();
+		this.body.empty();
+		this.set_body();
+		this.fields_list.on("click", () => this.show_list_settings());
+	}
+ //v2
+
+ get_fields_in_list_view() {
+	return this.meta.fields.filter((df) => {
+		return (
+			(frappe.model.is_value_type(df.fieldtype) &&
+				df.in_list_view &&
+				frappe.perm.has_perm(this.document_type, df.permlevel, "read")) ||
+			(df.fieldtype === "Currency" && df.options && !df.options.includes(":")) ||
+			df.fieldname === "status"
+		);
+	});
+}
+
+	async setup_columns() {
+		// setup columns for list view
+		this.columns = [];
+		const get_df = frappe.meta.get_docfield.bind(null, this.document_type);
+		// 1st column: title_field or name
+		if (this.meta?.title_field) {
+			this.columns.push({
+				type: "Subject",
+				df: get_df(this.meta.title_field),
+			});
+		} else {
+			this.columns.push({
+				type: "Subject",
+				df: {
+					label: __("ID"),
+					fieldname: "name",
+				},
+			});
+		}
+		// this.columns.push({
+		// 	type: "Tag",
+		// });
+
+		// 2nd column: Status indicator
+		if (frappe.has_indicator(this.document_type)) {
+			// indicator
+			this.columns.push({
+				type: "Status",
+				df: {
+					label: __("Status"),
+					fieldname: "status",
+				},
+			});
+		}
+		
+		const fields_in_list_view = this.get_fields_in_list_view();
+		// Add rest from in_list_view docfields
+		this.columns = this.columns.concat(
+			fields_in_list_view
+				.filter((df) => {
+					if (frappe.has_indicator(this.document_type) && df.fieldname === "status") {
+						return false;
+					}
+					if (!df.in_list_view) {
+						return false;
+					}
+					return df.fieldname !== this.meta.title_field;
+				})
+				.map((df) => ({
+					type: "Field",
+					df,
+				}))
+		);
+		
+		if (this.list_view_settings.fields) {
+			this.columns = this.reorder_listview_fields();
+		}
+
+		// limit max to 8 columns if no total_fields is set in List View Settings
+		// Screen with low density no of columns 4
+		// Screen with medium density no of columns 6
+		// Screen with high density no of columns 8
+		let total_fields = 6;
+
+		if (window.innerWidth <= 1366) {
+			total_fields = 4;
+		} else if (window.innerWidth >= 1920) {
+			total_fields = 10;
+		}
+
+		this.columns = this.columns.slice(0, this.list_view_settings.total_fields || total_fields);
+
+		if (
+			this.meta.title_field &&
+			this.meta.title_field !== "name"
+		) {
+			this.columns.push({
+				type: "Field",
+				df: {
+					label: __("ID"),
+					fieldname: "name",
+				},
+			});
+		}
+	}
+
+	reorder_listview_fields() {
+		let fields_order = [];
+		let fields = JSON.parse(this.list_view_settings.fields);
+
+		//title and tags field is fixed
+		fields_order.push(this.columns[0]);
+		fields_order.push(this.columns[1]);
+		this.columns.splice(0, 2);
+
+		for (let fld in fields) {
+			for (let col in this.columns) {
+				let field = fields[fld];
+				let column = this.columns[col];
+
+				if (column.type == "Status" && field.fieldname == "status_field") {
+					fields_order.push(column);
+					break;
+				} else if (column.type == "Field" && field.fieldname === column.df.fieldname) {
+					fields_order.push(column);
+					break;
+				}
+			}
+		}
+
+		return fields_order;
+	}
+
+	get_list_view_settings() {
+		return frappe
+			.call("frappe.desk.listview.get_list_settings", {
+				doctype: this.document_type,
+			})
+			.then((doc) => (this.list_view_settings = doc.message || {}));
+	}
+
+	refresh_columns(meta, list_view_settings) {
+		this.meta = meta;
+		this.list_view_settings = list_view_settings;
+
+		this.setup_columns().then(()=>{
+			this.body.empty();
+			this.set_body();
+		});
+	}
+
+	get_table_columns(){
+		if (this.columns) {
+			let columns = this.columns.reduce((acc, col) => {
+				if(col.type === "Field" || col.type === "Subject") {
+					return [...acc, col.df.fieldname];
+				} else {
+					return acc;
+				}
+			}, []);
+			console.log("columns", columns);
+			return columns;
+		} else {
+			let fields = ["name"];
+
+			// get name of title field
+			if (!this.title_field_name) {
+				this.meta = frappe.get_meta(this.document_type);
+				let meta = this.meta;
+				this.title_field_name = (meta && meta.title_field) || "name";
+			}
+
+			if (this.title_field_name && this.title_field_name != "name") {
+				fields.push(this.title_field_name);
+			}
+
+			// check doctype has status field
+			this.has_status_field = frappe.meta.has_field(this.document_type, "status");
+
+			if (this.has_status_field) {
+				fields.push("status");
+				fields.push("docstatus");
+
+				// add workflow state field if workflow exist & is active
+				let workflow_fieldname = frappe.workflow.get_state_fieldname(this.document_type);
+				workflow_fieldname && fields.push(workflow_fieldname);
+			}
+
+			fields.push("modified");
+
+			return fields;
+		}
+	}
+
 	setup_filter_list_button() {
 		this.filter_list = $(
 			`<div class="filter-list btn btn-xs pull-right" title="${__("Add/Update Filter")}">
@@ -87,6 +294,16 @@ export default class QuickListWidget extends Widget {
 		frappe.model.with_doctype(doctype, () => {
 			this.filter_group.add_filters_to_filter_group(this.filters);
 			this.dialog.set_df_property("filter_area", "hidden", false);
+		});
+	}
+	show_list_settings() {
+		frappe.model.with_doctype(this.document_type, () => {
+			new ListSettings({
+				listview: this,
+				doctype: this.document_type,
+				settings: this.list_view_settings,
+				meta: frappe.get_meta(this.document_type),
+			});
 		});
 	}
 
@@ -132,24 +349,24 @@ export default class QuickListWidget extends Widget {
 		this.loading.appendTo(this.body);
 	}
 
-	setup_quick_list_item(doc) {
+	setup_quick_list_item(doc) { 
 		let $quick_list_item = $(`
-			<div class="quick-list-item">
-				<div class="ellipsis left">
-					<div class="ellipsis title"
-						title="${strip_html(doc[this.title_field_name])}">
-						${strip_html(doc[this.title_field_name])}
-					</div>
-					<div class="timestamp text-muted">
-						${frappe.datetime.prettyDate(doc.modified)}
-					</div>
-				</div>
-			</div>
+			<tr class="quick-list-item">
+				${Object.keys(doc).map((field) => {
+					return `
+					<td>
+						${doc[field]}
+					</td>`
+				})}
+				<td>
+					<div class="right-arrow">${frappe.utils.icon("right", "xs")}</div>
+				</td>
+			</tr>
 		`);
 
-		$(`<div class="right-arrow">${frappe.utils.icon("right", "xs")}</div>`).appendTo(
-			$quick_list_item
-		);
+		// $(`<div class="right-arrow">${frappe.utils.icon("right", "xs")}</div>`).appendTo(
+		// 	$quick_list_item
+		// );
 
 		$quick_list_item.click((e) => {
 			if (e.ctrlKey || e.metaKey) {
@@ -167,39 +384,15 @@ export default class QuickListWidget extends Widget {
 		this.render_loading_state();
 
 		frappe.model.with_doctype(this.document_type, () => {
-			let fields = ["name"];
-
-			// get name of title field
-			if (!this.title_field_name) {
-				let meta = frappe.get_meta(this.document_type);
-				this.title_field_name = (meta && meta.title_field) || "name";
-			}
-
-			if (this.title_field_name && this.title_field_name != "name") {
-				fields.push(this.title_field_name);
-			}
-
-			// check doctype has status field
-			this.has_status_field = frappe.meta.has_field(this.document_type, "status");
-
-			if (this.has_status_field) {
-				fields.push("status");
-				fields.push("docstatus");
-
-				// add workflow state field if workflow exist & is active
-				let workflow_fieldname = frappe.workflow.get_state_fieldname(this.document_type);
-				workflow_fieldname && fields.push(workflow_fieldname);
-			}
-
-			fields.push("modified");
-
 			let quick_list_filter = frappe.utils.process_filter_expression(this.quick_list_filter);
+
+			this.field_keys = this.get_table_columns()
 
 			let args = {
 				method: "frappe.desk.reportview.get",
 				args: {
 					doctype: this.document_type,
-					fields: fields,
+					fields: this.field_keys,
 					filters: quick_list_filter,
 					order_by: "modified desc",
 					start: 0,
@@ -219,10 +412,36 @@ export default class QuickListWidget extends Widget {
 					return;
 				}
 
+				this.labels = r.message?.keys.map((key) => {
+					let column = this.columns.find((col) => col.df.fieldname === key);
+					if (column) {
+						return column.df.label;
+					} else {
+						return key.charAt(0).toUpperCase() + key.slice(1);
+					}
+				})
+
 				this.quick_list = data.map((doc) => this.setup_quick_list_item(doc));
+				this.quick_list_table = $(`
+				<table>
+					<tr class="quick-list-item">
+					${this.labels.map((field) => {
+						return `
+						<th>${field}</th>
+						`
+					})}
+					<td></td>
+					</tr>
+				</table>
+				`);
+
+				// this.quick_list.forEach(($quick_list_item) =>
+				// 	$quick_list_item.appendTo(this.body)
+				// );
 				this.quick_list.forEach(($quick_list_item) =>
-					$quick_list_item.appendTo(this.body)
+					$quick_list_item.appendTo(this.quick_list_table)
 				);
+				this.quick_list_table.appendTo(this.body);
 			});
 		});
 	}
